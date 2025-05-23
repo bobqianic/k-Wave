a807205edfce4e6ffbb204b2f9ae5e98487911d2
bobqianic <129547291+bobqianic@users.noreply.github.com>
2025-05-23 19:36:58 +0100

    Update compile.sh

--- HPC_example/compile.sh
+++ HPC_example/compile.sh
@@ -9,8 +9,9 @@ module load buildenv/default-foss-2023a-CUDA-12.1.1
 module load HDF5/1.14.0-gompi-2023a
 module load gompi/2023a
 
-# Copy input file to $TMPDIR
-cp -r $HOME/kspaceFirstOrder-CUDA $TMPDIR/
+# Copy source + input file to $TMPDIR
+cp -r $HOME/k-Wave-EasyBuild-Imperial/kspaceFirstOrder-CUDA $TMPDIR/
+cp -r $HOME/k-Wave-EasyBuild-Imperial/HPC_example/input_100_ffca869b-dad8-4bba-b940-97a7fab8a594.h5 $TMPDIR/kspaceFirstOrder-CUDA
 
 # Compile
 cd $TMPDIR/kspaceFirstOrder-CUDA
